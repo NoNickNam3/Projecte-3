f52a2f686b3617ff3e9e05de6b3e627a2f6abd5b
NoNickNam3 <jospugabus@gmail.com>
2023-04-19 20:00:39 +0200

    Afegeixo projecte d'app i poso noms més clars

---
+++ App_Projecte_3/app/src/main/java/org/milaifontanals/projecte3/MainActivity.java
@@ -0,0 +1,27 @@
+package org.milaifontanals.projecte3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        obreMaps();
+
+    }
+
+    private void obreMaps() {
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=48.852864,7.939082&via=47.853355, 7.936379");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+
+}
